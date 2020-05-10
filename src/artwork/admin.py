a2809fccbbc4a99704a2ccfc2e40e50f7ff77b5d
K3nny13 <kenneth_cogzell@hotmail.com>
2020-05-10 20:19:18 +0200

    Added a footer

--- src/artwork/admin.py
+++ src/artwork/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Artwork
 
 # Register your models here.
+
+class ArtworkAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Artwork, ArtworkAdmin)
--- src/artwork/models.py
+++ src/artwork/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Artwork(models.Model):
+    title = models.CharField(max_length=40)
+    image_url = models.CharField(max_length=350)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return this.title
